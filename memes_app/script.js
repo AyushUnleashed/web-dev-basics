2e23ed750adb6d4697a4d6712c0d9b414a0ebe59
Ayush Yadav <ayushyadav@Ayushs-MacBook-Pro.local>
2024-01-24 03:35:12 +0530

    memes app

---
+++ memes_app/script.js
@@ -0,0 +1,21 @@
+const memeContent = document.getElementById("memeContent")
+const imageContent = document.getElementById("imageContnet")
+
+
+const loadMeme = (title, image_url) => {
+    memeContent.innerHTML = title
+    imageContent.src =image_url
+}
+
+url = "https://meme-api.com/gimme"
+
+const fetchMeme =async () =>{
+    const response = await fetch(url)
+    const data =  await response.json()
+    const image_url = data["url"]
+    const title = data["title"]
+
+    loadMeme(title, image_url)
+}
+
+
